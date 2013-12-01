d919cf2d8b4a3e592650d57d645a166fcbbf50a2
mirgags <markmiraglia@gmail.com>
2013-12-01 10:31:45 -0800

    stuff done

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,20 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Resume::Application.config.secret_token = '73f52518dc5d86ba911cc6b392c2d04ffc8fcba41026932fa45ba19fbfdcdcd0369e8f09c4fcfc6378337068053ebb76b8fc7491c0bae2793fdff27d0f8c3321'
+
+## changed from default to dynamic token gen as per rails tutorial.
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # use existing token
+    File.read(token_file).chomp
+  else
+    # generate new token and store it in token_file
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Resume::Application.config.secret_key_base = secure_token
